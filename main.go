adb17e2b23536bfafef3f758f21d0a2f89d9680d
HerryZeng <herry.zeng@gmail.com>
2023-05-15 23:12:49 +0800

    update flag

--- main.go
+++ main.go
@@ -6,10 +6,27 @@ import (
 )
 
 func main() {
-	var name string
-	flag.StringVar(&name, "name", "Go 语言编程之旅", "帮助信息")
-	flag.StringVar(&name, "n", "Go 语言编程之旅", "帮助信息")
 	flag.Parse()
+
+	args := flag.Args()
+	if len(args) <= 0 {
+		return
+	}
+	var name string
+	switch args[0] {
+	case "go":
+		goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+		goCmd.StringVar(&name, "name", "Go语言", "帮助信息")
+		_ = goCmd.Parse(args[1:])
+	case "php":
+		phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+		phpCmd.StringVar(&name, "name", "PHP语言", "帮助信息")
+		_ = phpCmd.Parse(args[1:])
+	}
+
+	//flag.StringVar(&name, "name", "Go 语言编程之旅", "帮助信息")
+	//flag.StringVar(&name, "n", "Go 语言编程之旅", "帮助信息")
+	//flag.Parse()
 
 	log.Printf("name: %s", name)
 }
