9db6e8665a4359d3d27c613c1308cea7ebf4c46d
HerryZeng <herry.zeng@gmail.com>
2023-05-16 13:59:36 +0800

    update word command parse

--- main.go
+++ main.go
@@ -1,32 +1,13 @@
 package main
 
 import (
-	"flag"
+	"github.com/HerryZeng/tour/cmd"
 	"log"
 )
 
 func main() {
-	flag.Parse()
-
-	args := flag.Args()
-	if len(args) <= 0 {
-		return
+	err := cmd.Execute()
+	if err != nil {
+		log.Fatalf("cmd.Execute err: %v", err)
 	}
-	var name string
-	switch args[0] {
-	case "go":
-		goCmd := flag.NewFlagSet("go", flag.ExitOnError)
-		goCmd.StringVar(&name, "name", "Go语言", "帮助信息")
-		_ = goCmd.Parse(args[1:])
-	case "php":
-		phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
-		phpCmd.StringVar(&name, "name", "PHP语言", "帮助信息")
-		_ = phpCmd.Parse(args[1:])
-	}
-
-	//flag.StringVar(&name, "name", "Go 语言编程之旅", "帮助信息")
-	//flag.StringVar(&name, "n", "Go 语言编程之旅", "帮助信息")
-	//flag.Parse()
-
-	log.Printf("name: %s", name)
 }
